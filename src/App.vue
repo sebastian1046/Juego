c6e890123b616ed61281460d5057c6f1e9c6ed1c
Sebastian Ponce <sebastianponce104@gmail.com>
2020-03-16 18:05:15 -0500

    Se hizo la primera parte del juego

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,66 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Los links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<template lang='pug'>
+  .app
+    .texto1(v-if="is1" @click="choose(1, 1)") T1 {{ st1 }}
+    .texto2(v-if="is2" @click="choose(2, 2)") T2 {{ st2 }}
+    .texto3(v-if="is3" @click="choose(1, 3)") T3 {{ st3 }}
+    .texto4(v-if="is4" @click="choose(2, 4)") T4 {{ st4 }}
+
 </template>
 
 <script>
 export default {
   name: 'app',
+
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'El primer mensaje',
+      t1: ['texto11', 'trxto 12'],
+      t11: ['texto111', 'trxto 112'],
+      t2: ['texto21', 'trxto 22'],
+      st1: 'texto1',
+      st2: 'trxto 2',
+      st3: '',
+      st4: '',
+      is1: true,
+      is2: true,
+      is3: false,
+      is4: false,
+      line: '',
+      count: 0
+    }
+  },
+
+  methods:{
+    choose(pos, texto){
+
+      this.is2 = !this.is2
+      this.is1 = !this.is1
+      this.is3 = !this.is3
+      this.is4 = !this.is4
+
+      
+      const nRespuesta = this[`t${this.line}${pos}`]
+      console.log(nRespuesta)
+
+        this.line = this.line + String(pos)
+
+      if(texto == 1 || texto == 2){
+        this.st3 = nRespuesta[0]
+        this.st4 = nRespuesta[1]
+      } else {
+        this.st1 = nRespuesta[0]
+        this.st2 = nRespuesta[1]
+      }
+  
     }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='stylus'>
+  .app
 
-h1, h2 {
-  font-weight: normal;
-}
+  .textoDercha
+    text-align center
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
